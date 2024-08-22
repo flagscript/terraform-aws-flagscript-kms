41ad46113d2fcbac02f05e7fa299dd90d0260824
Greg Kaestle <flagscript@gmail.com>
2024-08-22 06:34:45 -0700

    fix(resolves #7): Fix various references.

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,18 @@
 output "alias" {
   description = "Alias of the kms key."
-  value       = aws_kms_alias.symetric_key_alias.name
+  value       = aws_kms_alias.symmetric_key_alias.name
 }
 
 output "arn" {
   description = "The Amazon Resource Name (ARN) of the key."
   sensitive   = true
-  value       = aws_kms_key.kms_key.arn
+  value       = aws_kms_key.symmetric_key.arn
 }
 
 output "id" {
   description = "The globally unique identifier for the key"
   sensitive   = true
-  value       = aws_kms_key.kms_key.key_id
+  value       = aws_kms_key.symmetric_key.key_id
 }
 
 output "key_account_id" {
